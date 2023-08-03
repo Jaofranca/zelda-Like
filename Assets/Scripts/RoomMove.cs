8e07280cc2f2767f2f94d3b9ea909678666d5170
João Victor de França <joaovfranca1@gmail.com>
2023-08-03 19:58:10 -0300

    added new scenario

---
+++ Assets/Scripts/RoomMove.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMove : MonoBehaviour
+{
+    public Vector2 cameraChange;
+    public Vector3 playerChange;
+    private CameraMovement cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMovement>();
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            cam.minPosition += cameraChange;
+            cam.maxPosition += cameraChange;
+            other.transform.position += playerChange;
+            
+        }
+    }
+}
---
+++ Assets/overwold.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="overwold" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
+ <image source="art/Overworld.png" width="640" height="576"/>
+ <wangsets>
+  <wangset name="Conjunto Sem Nome" type="corner" tile="1121">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="1160" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1161" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="1162" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="1200" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="1201" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1202" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="1240" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="1241" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="1242" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="1280" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="1281" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="1320" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="1321" wangid="0,1,0,1,0,1,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
